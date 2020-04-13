1227742366930b75da98990a5c73ac65ee0a4faf
Andria RAZAKANDREMA <andriarzk@gmail.com>
2020-04-14 00:56:14 +0300

    Deuxième ajout

--- lib/03_basics.rb
+++ lib/03_basics.rb
@@ -1,1 +1,19 @@
+def who_is_bigger(a,b,c)
+  input = who_is_bigger.max
+  place = input.index
+  if place == 0
+    return " a is bigger"
+  elsif place == 1
+    return "b is bigger"
+  elsif place == 2
+    return "c is bigger"
+  else
+    return "nil detected"
+  end
+end
 
+def crazy_stuff(phrase)
+  phrase.to_str
+  return phrase.reverse.upcase.delete('T','A',L)
+
+end
--- spec/03_basics_spec.rb
+++ spec/03_basics_spec.rb
@@ -4,10 +4,18 @@ describe 'who is the biggest number' do
   # Hint: 'def who_is_bigger(a, b, c)'
   it 'tells me the biggest' do
     expect(who_is_bigger(84, 42, nil)).to eq("nil detected")
+  end
+  it 'tells me the biggest' do
     expect(who_is_bigger(nil, 42, 21)).to eq("nil detected")
-    expect(who_is_bigger(84, 42, 21)).to eq("a is bigger")
-    expect(who_is_bigger(42, 84, 21)).to eq("b is bigger")
-    expect(who_is_bigger(42, 21, 84)).to eq("c is bigger")
+  end
+  it 'tells me the biggest' do
+      expect(who_is_bigger(84, 42, 21)).to eq("a is bigger")
+  end
+  it 'tells me the biggest' do
+      expect(who_is_bigger(42, 84, 21)).to eq("b is bigger")
+  end
+  it 'tells me the biggest' do
+      expect(who_is_bigger(42, 21, 84)).to eq("c is bigger")
   end
 end
 
@@ -16,11 +24,13 @@ describe 'crazy stuff on strings' do
   # Hint: google ruby string
   it 'does crazy stuff on strings' do
     expect(reverse_upcase_noLTA("Tries this at Home, Kids"))
-      .to eq("SDIK ,EMOH  SIH SEIR")
-    expect(reverse_upcase_noLTA("Ponies loves carrots"))
-      .to eq("SORRC SEVO SEINOP")
-    expect(reverse_upcase_noLTA("qwertyuiopasdfghjkl;zxcvbn"))
-      .to eq("NBVCXZ;KJHGFDSPOIUYREWQ")
+      .to eq("SDIK ,EMOH  SIH SEIR") end
+  it 'does crazy stuff on strings' do
+      expect(reverse_upcase_noLTA("Ponies loves carrots"))
+      .to eq("SORRC SEVO SEINOP") end
+  it 'does crazy stuff on strings' do
+      expect(reverse_upcase_noLTA("qwertyuiopasdfghjkl;zxcvbn"))
+      .to eq("NBVCXZ;KJHGFDSPOIUYREWQ") 
   end
 end
 
